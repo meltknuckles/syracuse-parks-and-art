5bc2c067dc34ccc7681606f56ca30333002b8c03
melt <meltknuckles@gmail.com>
2024-05-22 17:01:52 -0400

    fix build issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/soho-light/theme.css';
