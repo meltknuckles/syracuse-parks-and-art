32a6fc011c289f8e6b6547657d028d82bf10b0b5
melt <meltknuckles@gmail.com>
2024-05-22 17:33:41 -0400

    comment out source-map-support

--- bin/syr-parks-cdk.ts
+++ bin/syr-parks-cdk.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
+// import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SlimeCenterCdkStack } from '../lib/syr-parks-cdk-stack';
 
