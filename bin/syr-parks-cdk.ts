2808e7e102099afd777573f99f34aa265eebd4d8
melt <meltknuckles@gmail.com>
2024-05-22 21:02:14 -0400

    bluh

--- bin/syr-parks-cdk.ts
+++ bin/syr-parks-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { SyracuseParksCdkStack } from '../lib/syr-parks-cdk-stack.ts';
+import { SyracuseParksCdkStack } from '../lib/syr-parks-cdk-stack';
 
 const app = new cdk.App();
 new SyracuseParksCdkStack(app, 'SyracuseParksCdkStack', {
